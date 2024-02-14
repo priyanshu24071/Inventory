2d16876e88e3ed962294c0654f731b019de1281f
Priyanshu Raj Singh <72143359+priyanshu24071@users.noreply.github.com>
2024-02-14 21:31:48 +0530

    Update publisher.py

--- publisher.py
+++ publisher.py
@@ -4,9 +4,9 @@ import time
 import logging
 
 logging.basicConfig(level=logging.INFO)
-
+n=12
 with DaprClient() as client:
-    for i in range(1, 10):
+    for i in range(1, n):
         item = {'itemId': i}
         # Publish an event/message using Dapr PubSub
         result = client.publish_event(
